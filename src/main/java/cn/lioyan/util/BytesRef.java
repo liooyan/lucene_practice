2edca4752d3cccc7094741ce0868c179beab6500
李延 <liyan@wondersoft.cn>
2023-07-17 16:55:58 +0800

    添加  BytesRef

---
+++ src/main/java/cn/lioyan/util/BytesRef.java
@@ -0,0 +1,14 @@
+package cn.lioyan.util;
+
+/**
+ * {@link BytesRef}
+ * byte 数组的引用。
+ * 用于表示一个 byte 数组，可以通过 offset 和 length 来表示一个 byte 数组的子数组。
+ *
+ * 通常 使用同一个byte数组， 然后通过不同的 offset 和 length 来表示不同的子数组。
+ * @author com.lioyan
+ * @since 2023/7/17  16:54
+ */
+public class BytesRef
+{
+}
