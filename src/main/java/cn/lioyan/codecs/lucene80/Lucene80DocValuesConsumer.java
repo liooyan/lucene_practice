f880acc7af08c6612cb6c714939da30ce5662d90
李延 <liyan@wondersoft.cn>
2023-07-19 18:21:43 +0800

    开始 Lucene80DocValuesConsumer

--- src/main/java/cn/lioyan/codecs/lucene80/Lucene80DocValuesConsumer.java
+++ src/main/java/cn/lioyan/codecs/lucene80/Lucene80DocValuesConsumer.java
@@ -3,7 +3,7 @@ package cn.lioyan.codecs.lucene80;
 /**
  * {@link org.apache.lucene.codecs.lucene80.Lucene80DocValuesConsumer}
  * 8.0 版本的 DocValuesConsumer ，
- *
+ * 对于文档的id 通过{@link IndexedDISI} 存储， 值有自己不同的实现。
  * @author com.lioyan
  * @since 2023/7/19  16:39
  */
