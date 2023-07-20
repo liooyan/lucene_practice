3f2823723b14a977d2a0ab2ec8d441ba5617406b
李延 <liyan@wondersoft.cn>
2023-07-20 17:39:19 +0800

    开始 IndexWriter

---
+++ src/main/java/cn/lioyan/index/IndexWriter.java
@@ -0,0 +1,17 @@
+package cn.lioyan.index;
+
+/**
+ * {@link org.apache.lucene.index.IndexWriter}
+ * 维护索引的所有 编辑操作
+ * 构造函数创建：{@link DocumentsWriter}
+ *
+ *
+ * 1. addDocument 添加文档
+ *
+
+ * @author com.lioyan
+ * @since 2023/7/20  16:58
+ */
+public class IndexWriter
+{
+}
