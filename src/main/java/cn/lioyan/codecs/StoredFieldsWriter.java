5c27496ca036290a273baef0e1d9fa0fa2bbb45b
李延 <liyan@wondersoft.cn>
2023-07-20 09:54:53 +0800

    完善 StoredFieldsWriter

---
+++ src/main/java/cn/lioyan/codecs/StoredFieldsWriter.java
@@ -0,0 +1,12 @@
+package cn.lioyan.codecs;
+
+/**
+ * {@link org.apache.lucene.codecs.StoredFieldsWriter}
+ * 用于存储原始字段的类
+ *
+ * @author com.lioyan
+ * @since 2023/7/20  9:38
+ */
+public class StoredFieldsWriter
+{
+}
---
+++ src/main/java/cn/lioyan/codecs/compressing/CompressingStoredFieldsWriter.java
@@ -0,0 +1,11 @@
+package cn.lioyan.codecs.compressing;
+
+/**
+ * {@link org.apache.lucene.codecs.compressing.CompressingStoredFieldsWriter}
+ * StoredFieldsWriter 的具体实现，
+ * @author com.lioyan
+ * @since 2023/7/20  9:54
+ */
+public class CompressingStoredFieldsWriter
+{
+}
