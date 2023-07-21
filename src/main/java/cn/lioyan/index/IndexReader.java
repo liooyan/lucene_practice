bb772b1296b059aec36f8cad3af0cc897d3242fb
李延 <liyan@wondersoft.cn>
2023-07-21 18:32:12 +0800

    添加 IndexReader

---
+++ src/main/java/cn/lioyan/index/IndexReader.java
@@ -0,0 +1,17 @@
+package cn.lioyan.index;
+
+import org.apache.lucene.index.CompositeReader;
+
+
+/**
+ * {@link org.apache.lucene.index.IndexReader}
+ * 索引加载器，分为两个主要子类
+ *
+ * 1、{@link  LeafReader} 单个段的加载
+ * 2、{@link  CompositeReader} 多个段的加载
+ * @author com.lioyan
+ * @since 2023/7/21  18:27
+ */
+public class IndexReader
+{
+}
