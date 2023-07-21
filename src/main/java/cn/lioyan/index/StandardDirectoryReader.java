8caa5a33f68b630ad4be0e2cfc9982cb2586291c
李延 <liyan@wondersoft.cn>
2023-07-21 18:47:18 +0800

    补充 StandardDirectoryReader

--- src/main/java/cn/lioyan/index/StandardDirectoryReader.java
+++ src/main/java/cn/lioyan/index/StandardDirectoryReader.java
@@ -1,8 +1,19 @@
 package cn.lioyan.index;
 
+import org.apache.lucene.index.SegmentReader;
+import org.apache.lucene.store.Directory;
+
+
 /**
  * {@link org.apache.lucene.index.StandardDirectoryReader}
  *
+ * 读取指定文件夹下的索引
+ * {@link org.apache.lucene.index.StandardDirectoryReader#open(Directory)}
+ *
+ * 加载所有的段
+ *   SegmentInfos sis = SegmentInfos.readCommit(directory, segmentFileName);
+ *
+ *   为每个段 创建 {@link SegmentReader}
  * @author com.lioyan
  * @since 2023/7/21  11:47
  */
