ddb229c05a45fbaa58df2a5d5de67eae5c1789c9
李延 <liyan@wondersoft.cn>
2023-07-24 11:11:56 +0800

    修改 DocValueTest 测试类

--- src/main/java/cn/lioyan/test/DocValueTest.java
+++ src/main/java/cn/lioyan/test/DocValueTest.java
@@ -35,6 +35,38 @@ public class DocValueTest
     public static void MAIN()
         throws IOException
     {
+        //addValue();
+        tremQuert();
+
+        //        // 3. search
+        //        int hitsPerPage = 10;
+        //        IndexReader reader = DirectoryReader.open(directory);
+        //        IndexSearcher searcher = new IndexSearcher(reader);
+        //        List<LeafReaderContext> leaves = reader.getContext().leaves();
+        //
+        //        Weight weight = q.createWeight(searcher, ScoreMode.COMPLETE_NO_SCORES, 1);
+        //        //        weight.scorer(reader.getContext());
+        //        //        weight.
+        //
+        //        for (LeafReaderContext leaf : leaves)
+        //        {
+        //            Scorer scorer = weight.scorer(leaf);
+        //            if (scorer == null)
+        //            {
+        //                continue;
+        //            }
+        //            DocIdSetIterator iterator = scorer.iterator();
+        //            int doc;
+        //            while ((doc = iterator.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS)
+        //            {
+        //                System.out.println(doc);
+        //            }
+        //        }
+    }
+
+    private static void addValue()
+        throws IOException
+    {
         //        // 0. Specify the analyzer for tokenizing text.
         //        //    The same analyzer should be used for indexing and searching
         StandardAnalyzer analyzer = new StandardAnalyzer();
@@ -44,67 +76,22 @@ public class DocValueTest
 
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
         config.setUseCompoundFile(false);
-        numericQuert();
-        //        IndexWriter w = new IndexWriter(directory, config);
-        //        for (int i = 0; i < 200000; i++)
-        //        {
-        //            addDoc(w, "Lucene Lucene in Action", "193398817", -5, new int[] {1, 2}, new String[] {"los angles", "beijing"});
-        //            addDoc(w, "Lucene for Dummies", "55320055Z", 4, new int[] {5, 1}, new String[] {"shanghai", "beijing"});
-        //            addDoc(w, "Managing Gigabytes", "55063554A", 12, new int[] {0, 1, 2}, new String[] {"shenzhen", "guangzhou"});
-        //            addDoc(w, "The Art of Computer Science", "9900333X", 2, new int[] {10, 4, 3}, new String[] {"shanghai", "los angles"});
-        //            addDoc(w, "C++ Primer", "914324235", 11, new int[] {0, 5, 2, 3}, new String[] {"beijing", "shenzhen"});
-        //            addDoc(w, "I like Lucene", "fdsjfa2313", 1, new int[] {0, 1, 2, 4}, new String[] {"nanjing", "tianjin"});
-        //            addDoc(w, "Lucene and C++ Primer", "fdsfaf", 10, new int[] {0, 1, 2}, new String[] {"shenzhen", "guangzhou"});
-        //            addDoc(w, "C++ api", "411223432", 2, new int[] {0, 11, 2}, new String[] {"shenzhen", "shanghai"});
-        //            addDoc(w, "C++ Primer", "914324236", 50, new int[] {3, 2, 6, 1}, new String[] {"beijing"});
-        //
-        //        }
-        //        w.close();
+        IndexWriter w = new IndexWriter(directory, config);
+        for (int i = 0; i < 200000; i++)
+        {
+            addDoc(w, "Lucene Lucene in Action", "193398817", -5, new int[] {1, 2}, new String[] {"los angles", "beijing"});
+            addDoc(w, "Lucene for Dummies", "55320055Z", 4, new int[] {5, 1}, new String[] {"shanghai", "beijing"});
+            addDoc(w, "Managing Gigabytes", "55063554A", 12, new int[] {0, 1, 2}, new String[] {"shenzhen", "guangzhou"});
+            addDoc(w, "The Art of Computer Science", "9900333X", 2, new int[] {10, 4, 3}, new String[] {"shanghai", "los angles"});
+            addDoc(w, "C++ Primer", "914324235", 11, new int[] {0, 5, 2, 3}, new String[] {"beijing", "shenzhen"});
+            addDoc(w, "I like Lucene", "fdsjfa2313", 1, new int[] {0, 1, 2, 4}, new String[] {"nanjing", "tianjin"});
+            addDoc(w, "Lucene and C++ Primer", "fdsfaf", 10, new int[] {0, 1, 2}, new String[] {"shenzhen", "guangzhou"});
+            addDoc(w, "C++ api", "411223432", 2, new int[] {0, 11, 2}, new String[] {"shenzhen", "shanghai"});
+            addDoc(w, "C++ Primer", "914324236", 50, new int[] {3, 2, 6, 1}, new String[] {"beijing"});
 
-        // 2. query
-        String querystr = "primer";
+        }
+        w.close();
 
-        // the "title" arg specifies the default field to use
-        // when no field is explicitly specified in the query.
-        Query q = NumericDocValuesField.newSlowRangeQuery("visit", 32, 66);
-        //        Query q2 = SortedNumericDocValuesField.newSlowRangeQuery("sale", 0, 10);
-        //        //        Query q2 = new TermQuery(new Term("title", querystr));
-        //        //         BooleanQuery query = new BooleanQuery();
-        //        //        MatchAllDocsQuery q = new MatchAllDocsQuery();
-        //
-        //        BooleanQuery.Builder b = new BooleanQuery.Builder();
-        //        b.add(q, BooleanClause.Occur.SHOULD);
-        //        b.add(q2, BooleanClause.Occur.MUST_NOT);
-        //        b.setMinimumNumberShouldMatch(1);
-        //        BooleanQuery query = b.build();
-        //        //sort
-        //        SortField visitSort = new SortedNumericSortField("visit", SortField.Type.INT, true);
-        //        Sort sort = new Sort();
-
-//        // 3. search
-//        int hitsPerPage = 10;
-//        IndexReader reader = DirectoryReader.open(directory);
-//        IndexSearcher searcher = new IndexSearcher(reader);
-//        List<LeafReaderContext> leaves = reader.getContext().leaves();
-//
-//        Weight weight = q.createWeight(searcher, ScoreMode.COMPLETE_NO_SCORES, 1);
-//        //        weight.scorer(reader.getContext());
-//        //        weight.
-//
-//        for (LeafReaderContext leaf : leaves)
-//        {
-//            Scorer scorer = weight.scorer(leaf);
-//            if (scorer == null)
-//            {
-//                continue;
-//            }
-//            DocIdSetIterator iterator = scorer.iterator();
-//            int doc;
-//            while ((doc = iterator.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS)
-//            {
-//                System.out.println(doc);
-//            }
-//        }
     }
 
     private static void addDoc(IndexWriter w, String title, String isbn, int visit, int[] sale_list, String[] locations)
@@ -135,6 +122,32 @@ public class DocValueTest
         }
 
         w.addDocument(doc);
+    }
+
+    private static void tremQuert()
+        throws IOException
+    {
+        long startTime = System.currentTimeMillis();
+        Sort sort = new Sort();
+        Directory directory = FSDirectory.open(Paths.get("tempPath"));
+
+        IndexReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+
+        Query q = new TermQuery(new Term("isbn", "fdsjfa2313"));
+
+        TopFieldDocs search = searcher.search(q, 10, sort);
+        ScoreDoc[] hits = search.scoreDocs;
+        // 4. display results
+        System.out.println("Found " + hits.length + " hits.");
+        for (int j = 0; j < hits.length; ++j)
+        {
+            int docId = hits[j].doc;
+            Document d = searcher.doc(docId);
+            System.out.println(docId + ". " + d.get("isbn") + "\t" + d.get("title") + "\t" + d.get("visit"));
+        }
+        System.out.println("查询时间：" + (System.currentTimeMillis() - startTime));
+
     }
 
     private static void numericQuert()
@@ -180,8 +193,6 @@ public class DocValueTest
         b.add(q2, BooleanClause.Occur.MUST);
         b.setMinimumNumberShouldMatch(1);
         BooleanQuery query = b.build();
-
-
 
         TopFieldDocs search = searcher.search(query, 10, sort);
         ScoreDoc[] hits = search.scoreDocs;
